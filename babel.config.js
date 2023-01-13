23c069ae049399432c580179c67bb941370cc498
Justine Ballares <ballaresjustine@gmail.com>
2023-01-14 06:14:32 +0800

    Updated babel config for absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@assets': './src/assets',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@redux': './src/redux',
+          '@services': './src/services',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+          '@i18n': './src/i18n',
+          '@lib': './src/lib',
+          '@interfaces': './src/interfaces',
+        },
+      },
+    ],
+  ],
 };
